22d941c97bc3955afb087ec8eae08474b8214f00
ChaoticlyLowIQ <82467920+ChaoticlyLowIQ@users.noreply.github.com>
2021-04-20 09:14:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,25 @@
 namespace SpriteKind {
     export const hero = SpriteKind.create()
+    export const money = SpriteKind.create()
+    export const ground = SpriteKind.create()
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    jeff.vy = -100
-    pause(200)
-    jeff.vy = 100
-    pause(200)
+    jeff.vy = -80
+    pause(300)
+    jeff.vy = 80
+    pause(300)
     jeff.vy = 0
+})
+sprites.onOverlap(SpriteKind.hero, SpriteKind.Enemy, function (sprite, otherSprite) {
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 500)
+    sprite.x += -10
+})
+sprites.onOverlap(SpriteKind.hero, SpriteKind.money, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.spray, 100)
+    info.changeScoreBy(1)
+    music.baDing.play()
 })
 let jeff: Sprite = null
 scene.setBackgroundColor(9)
@@ -29,6 +42,53 @@ jeff = sprites.create(img`
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.hero)
 jeff.setPosition(10, 70)
+let badguy = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .......fb111111bf.......
+    ......fffcdb1bdffff.....
+    ....fc111cbfbfc111cf....
+    ....f1b1b1ffff1b1b1f....
+    ....fbfbffffffbfbfbf....
+    .........ffffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.Enemy)
+badguy.setPosition(100, 70)
+info.setLife(3)
+let coin = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . 5 5 5 5 5 . . . . . . 
+    . . . . 5 5 5 4 5 5 5 . . . . . 
+    . . . . 5 5 5 4 5 5 5 . . . . . 
+    . . . . 5 5 5 4 5 5 5 . . . . . 
+    . . . . 5 5 5 4 5 5 5 . . . . . 
+    . . . . 5 5 5 4 5 5 5 . . . . . 
+    . . . . . 5 5 5 5 5 . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.money)
+info.setScore(0)
 game.onUpdate(function () {
     jeff.x += controller.dx()
 })
