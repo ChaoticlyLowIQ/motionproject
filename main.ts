600fa74f3694d62ce8a3eb9962ad52a23eb33227
ChaoticlyLowIQ <82467920+ChaoticlyLowIQ@users.noreply.github.com>
2021-04-21 14:13:19 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -6,59 +6,45 @@ namespace SpriteKind {
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     bird.vy = -100
-    bird.vx = 50
 })
-sprites.onOverlap(SpriteKind.bird, SpriteKind.money, function (sprite, otherSprite) {
-    otherSprite.destroy(effects.spray, 100)
-    info.changeScoreBy(1)
-    music.baDing.play()
+scene.onOverlapTile(SpriteKind.bird, assets.tile`myTile0`, function (sprite, location) {
+    game.over(true)
+})
+scene.onHitWall(SpriteKind.bird, function (sprite, location) {
+    info.changeLifeBy(-1)
+    bird.setPosition(70, 50)
 })
 let bird: Sprite = null
+game.splash("press space to jump")
 scene.setBackgroundColor(9)
 bird = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
     `, SpriteKind.bird)
-bird.setPosition(10, 70)
 bird.ay = 300
+bird.vx = 35
 info.setLife(3)
-let coin = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . 5 5 5 5 5 . . . . . . 
-    . . . . 5 5 5 4 5 5 5 . . . . . 
-    . . . . 5 5 5 4 5 5 5 . . . . . 
-    . . . . 5 5 5 4 5 5 5 . . . . . 
-    . . . . 5 5 5 4 5 5 5 . . . . . 
-    . . . . 5 5 5 4 5 5 5 . . . . . 
-    . . . . . 5 5 5 5 5 . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.money)
-info.setScore(0)
 scene.cameraFollowSprite(bird)
 tiles.setTilemap(tilemap`level2`)
 game.onUpdate(function () {
     if (bird.bottom > 159 || bird.top < 1) {
         game.over(false)
+    }
+    if (bird.vx < 35) {
+        bird.vx = 35
     }
 })
